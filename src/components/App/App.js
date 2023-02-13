a4568fe935627db251bb014ba290816cefe53b65
ElMoscaviador <aurelien.flieger@protonmail.com>
2023-02-13 18:21:06 +0100

    Initialize repository
    This repository is set up.

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import Header from "../Header/Header";
+import "../../styles/App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import App from "./components/App/App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const documentRoot = ReactDOM.createRoot(document.querySelector(".root"));
+documentRoot.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
